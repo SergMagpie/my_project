43fb5d59982051108c09e89fece3516f593ad37c
SergMagpie <77747152+SergMagpie@users.noreply.github.com>
2021-06-12 21:03:49 +0300

    up

--- my_site/itstep/views.py
+++ my_site/itstep/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('ItStep page')
+    return render(request, "itstep/index.html")
+
+def exercises(request):
+    return HttpResponse(f'Articles by exercises')
+
+def archive(request, cat):
+    return HttpResponse(f'Archive by {cat}')
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Page not found :(</h1>')
